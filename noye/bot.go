c8cac5b49adf0bb8567039efacaeb0d0fbfd7c5a
j6n <genivf@outlook.com>
2014-02-07 10:41:54 -0500

    added some spacing for logical grouping

--- noye/bot.go
+++ noye/bot.go
@@ -2,10 +2,13 @@ package noye
 
 type Bot interface {
 	Dial(addr, nick, user string) error
+	Close()
+
 	Send(f string, a ...interface{})
 	Privmsg(target, msg string)
+
 	Join(target string)
 	Part(target string)
-	Close()
+
 	Wait() <-chan struct{}
 }
--- noye/conn.go
+++ noye/conn.go
@@ -3,6 +3,7 @@ package noye
 type Conn interface {
 	Dial(addr, user string) error
 	Close()
+
 	WriteLine(raw string)
 	ReadLine() (string, error)
 }
