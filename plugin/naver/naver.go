eb86a35e69e8b49032a221441fd79053040bf71d
j6n <genivf@outlook.com>
2014-02-09 17:01:29 -0500

    added tvcast command to naver

--- plugin/naver/naver.go
+++ plugin/naver/naver.go
@@ -4,6 +4,8 @@ import (
 	"regexp"
 
 	"github.com/j6n/naver/music"
+	"github.com/j6n/naver/tvcast"
+	// for groups
 	"github.com/j6n/noye/noye"
 	"github.com/j6n/noye/plugin"
 )
@@ -18,17 +20,29 @@ func New() *Naver {
 	return naver
 }
 
+// http://tvcast.naver.com/v/42788
+
+var tvcastRe = regexp.MustCompile(`(?:tvcast.naver.com/v/(\d+)$)|(\d+)$`)
+
 func (n *Naver) process() {
-	music := plugin.Respond("naver", plugin.RegexMatcher(
+	music := plugin.Respond("music", plugin.RegexMatcher(
 		regexp.MustCompile(`(http://music.naver.com/.*?\S*)+`),
 		true,
 	))
 	music.Each = true
 
+	tvcast := plugin.Respond("tvcast", plugin.RegexMatcher(
+		tvcastRe,
+		true,
+	))
+	tvcast.Each = true
+
 	for msg := range n.Listen() {
 		switch {
 		case music.Match(msg):
 			n.handleMusic(msg, music.Results())
+		case tvcast.Match(msg):
+			n.handleTvcast(msg, tvcast.Results())
 		}
 	}
 }
@@ -44,12 +58,33 @@ func (n *Naver) handleMusic(msg noye.Message, match []string) {
 		for _, id := range ids {
 			vid, err := music.GetVideo(id)
 			if err != nil {
-				n.Error(msg, "music/findIDs", err)
+				n.Error(msg, "music: "+id, err)
 				continue
 			}
 
 			n.Reply(msg, "[%s] %s", vid.Encoding, vid.Title)
 			n.Reply(msg, "%s", vid.PlayUrl)
 		}
+	}
+}
+
+func (n *Naver) handleTvcast(msg noye.Message, matches []string) {
+	for _, match := range matches {
+		var id string
+		for _, p := range tvcastRe.FindStringSubmatch(match)[1:] {
+			if p != "" {
+				id = p
+				break
+			}
+		}
+
+		vid, err := tvcast.GetVideo(id)
+		if err != nil {
+			n.Error(msg, "tvcast: "+id, err)
+			continue
+		}
+
+		n.Reply(msg, "[%s] %s", vid.Encoding, vid.Title)
+		n.Reply(msg, "%s", vid.PlayUrl)
 	}
 }
--- plugin/naver/naver_test.go
+++ plugin/naver/naver_test.go
@@ -12,8 +12,8 @@ func TestNaver(t *testing.T) {
 	t.Parallel()
 
 	Convey("Naver plugin should", t, func() {
-		Convey("Handle a music command", func() {
-			Convey("With one url", func() {
+		Convey("handle a music command", func() {
+			Convey("with one url", func() {
 				bot, naver, res := mock.NewMockBot(), New(), make(chan string)
 				bot.PrivmsgFn = func(target, msg string) { res <- msg }
 
@@ -21,7 +21,7 @@ func TestNaver(t *testing.T) {
 				naver.Listen() <- noye.Message{
 					"museun",
 					"#museun",
-					"noye: naver music http://music.naver.com/promotion/specialContent.nhn?articleId=4569&page=2",
+					"noye: music http://music.naver.com/promotion/specialContent.nhn?articleId=4569&page=2",
 				}
 
 				So(<-res, ShouldEqual, "[720P] 안무 영상")
@@ -34,7 +34,7 @@ func TestNaver(t *testing.T) {
 				close(res)
 			})
 
-			Convey("With multiple urls", func() {
+			Convey("with multiple urls", func() {
 				bot, naver, res := mock.NewMockBot(), New(), make(chan string)
 				bot.PrivmsgFn = func(target, msg string) { res <- msg }
 
@@ -42,7 +42,7 @@ func TestNaver(t *testing.T) {
 				naver.Listen() <- noye.Message{
 					"museun",
 					"#museun",
-					"noye: naver music http://music.naver.com/promotion/specialContent.nhn?articleId=4569&page=2 " +
+					"noye: music http://music.naver.com/promotion/specialContent.nhn?articleId=4569&page=2 " +
 						"http://music.naver.com/promotion/specialContent.nhn?articleId=4557&page=2",
 				}
 
@@ -56,6 +56,80 @@ func TestNaver(t *testing.T) {
 				So(<-res, ShouldEqual, "[720P] 메이킹 영상")
 				So(<-res, ShouldNotBeNil) // the url
 				So(<-res, ShouldEqual, "[720P] 인사말 영상")
+				So(<-res, ShouldNotBeNil) // the url
+
+				close(res)
+			})
+		})
+
+		Convey("handle a tvcast command", func() {
+			Convey("with one url", func() {
+				bot, naver, res := mock.NewMockBot(), New(), make(chan string)
+				bot.PrivmsgFn = func(target, msg string) { res <- msg }
+
+				naver.Hook(bot)
+				naver.Listen() <- noye.Message{
+					"museun",
+					"#museun",
+					"noye: tvcast http://tvcast.naver.com/v/42788",
+				}
+
+				So(<-res, ShouldEqual, "[720P] [더스타] 걸스데이 소진, 대걸레 잡고 샤이니 '드림 걸' 안무시범 '폭소'")
+				So(<-res, ShouldNotBeNil) // the url
+
+				close(res)
+			})
+
+			Convey("with one id", func() {
+				bot, naver, res := mock.NewMockBot(), New(), make(chan string)
+				bot.PrivmsgFn = func(target, msg string) { res <- msg }
+
+				naver.Hook(bot)
+				naver.Listen() <- noye.Message{
+					"museun",
+					"#museun",
+					"noye: tvcast 42788",
+				}
+
+				So(<-res, ShouldEqual, "[720P] [더스타] 걸스데이 소진, 대걸레 잡고 샤이니 '드림 걸' 안무시범 '폭소'")
+				So(<-res, ShouldNotBeNil) // the url
+
+				close(res)
+			})
+
+			Convey("with multiple urls", func() {
+				bot, naver, res := mock.NewMockBot(), New(), make(chan string)
+				bot.PrivmsgFn = func(target, msg string) { res <- msg }
+
+				naver.Hook(bot)
+				naver.Listen() <- noye.Message{
+					"museun",
+					"#museun",
+					"noye: tvcast http://tvcast.naver.com/v/42788 http://tvcast.naver.com/v/42782",
+				}
+
+				So(<-res, ShouldEqual, "[720P] [더스타] 걸스데이 소진, 대걸레 잡고 샤이니 '드림 걸' 안무시범 '폭소'")
+				So(<-res, ShouldNotBeNil) // the url
+				So(<-res, ShouldEqual, "[720P] [더스타] 걸스데이 민아, 3콤보 라이브+웨이브+애교…'방만능'된 사연")
+				So(<-res, ShouldNotBeNil) // the url
+
+				close(res)
+			})
+
+			Convey("with urls and ids", func() {
+				bot, naver, res := mock.NewMockBot(), New(), make(chan string)
+				bot.PrivmsgFn = func(target, msg string) { res <- msg }
+
+				naver.Hook(bot)
+				naver.Listen() <- noye.Message{
+					"museun",
+					"#museun",
+					"noye: tvcast http://tvcast.naver.com/v/42788 42782",
+				}
+
+				So(<-res, ShouldEqual, "[720P] [더스타] 걸스데이 소진, 대걸레 잡고 샤이니 '드림 걸' 안무시범 '폭소'")
+				So(<-res, ShouldNotBeNil) // the url
+				So(<-res, ShouldEqual, "[720P] [더스타] 걸스데이 민아, 3콤보 라이브+웨이브+애교…'방만능'된 사연")
 				So(<-res, ShouldNotBeNil) // the url
 
 				close(res)
