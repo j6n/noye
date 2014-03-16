7939794119fee5ae3966af1f0ed5941ea564f1c4
j6n <genivf@outlook.com>
2014-03-16 02:20:11 -0400

    commented plugin.Base

--- plugin/base.go
+++ plugin/base.go
@@ -6,41 +6,55 @@ import (
 	"github.com/j6n/noye/noye"
 )
 
-type BasePlugin struct {
+// Base is a type to reduce boilerplate of plugins
+// It holds the noye.Bot reference, a channel to receive
+// noye.Messages on and a map of disabled things
+type Base struct {
 	Bot      noye.Bot
 	input    chan noye.Message
 	Disabled map[string]bool
 }
 
-func New() *BasePlugin {
-	return &BasePlugin{
+// New returns a new Base plugin
+func New() *Base {
+	return &Base{
 		input:    make(chan noye.Message),
 		Disabled: make(map[string]bool),
 	}
 }
 
-func (b *BasePlugin) Listen() chan noye.Message { return b.input }
+// Listen returns the message to receives messages upon
+func (b *Base) Listen() chan noye.Message {
+	return b.input
+}
 
-func (b *BasePlugin) Status(ch string) bool {
+// Status returns whether the input is disabled
+func (b *Base) Status(ch string) bool {
 	s, ok := b.Disabled[ch]
 	return s && ok
 }
 
-func (b *BasePlugin) SetStatus(ch string, status bool) { b.Disabled[ch] = status }
+// SetStatus disables the input
+func (b *Base) SetStatus(ch string, status bool) {
+	b.Disabled[ch] = status
+}
 
-func (b *BasePlugin) Hook(bot noye.Bot) { b.Bot = bot }
+// Hook sets the noye.Bot reference
+func (b *Base) Hook(bot noye.Bot) { b.Bot = bot }
 
-func (b *BasePlugin) Reply(msg noye.Message, f string, a ...interface{}) {
+// Reply replies to the noye.Message with a formatted string
+func (b *Base) Reply(msg noye.Message, f string, a ...interface{}) {
 	b.Bot.Privmsg(msg.Target, fmt.Sprintf(f, a...))
 }
 
-func (b *BasePlugin) Error(msg noye.Message, text string, err error) {
+// Error replies to the noye.Message with a readable error
+func (b *Base) Error(msg noye.Message, text string, err error) {
 	b.Reply(msg, "error with %s (%s)", text, err)
 }
 
 // type Handler func(noye.Message, []string)
 
-// func (b *BasePlugin) Handle(fn Handler, msg noye.Message, result []string) {
+// func (b *Base) Handle(fn Handler, msg noye.Message, result []string) {
 // 	defer func() { recover() }() // don't crash
 // 	fn(msg, result)
 // }
