399a78227a1caec5775ac5825871adab999357c5
j6n <genivf@outlook.com>
2014-03-16 01:53:32 -0400

    added comments for the mock bot

--- mock/bot.go
+++ mock/bot.go
@@ -2,6 +2,8 @@ package mock
 
 import "github.com/j6n/noye/noye"
 
+// MockBot is a type that implements noye.Bot
+// which can have parts of it overridden by changing the functors
 type MockBot struct {
 	SendFn    func(string, ...interface{})
 	PrivmsgFn func(string, string)
@@ -9,6 +11,7 @@ type MockBot struct {
 	PartFn    func(string)
 }
 
+// NewMockBot returns a new MockBot with the fns set to no-ops
 func NewMockBot() *MockBot {
 	return &MockBot{
 		func(string, ...interface{}) {},
@@ -18,17 +21,26 @@ func NewMockBot() *MockBot {
 	}
 }
 
+// Dial is just here for the interface
 func (m *MockBot) Dial(addr, nick, user string) error { return nil }
 
+// Close is just here for the interface
 func (m *MockBot) Close() {}
 
+// Send delegates to SendFn
 func (m *MockBot) Send(f string, a ...interface{}) { m.SendFn(f, a) }
 
+// Privmsg delegates to PrivmsgFn
 func (m *MockBot) Privmsg(target, msg string) { m.PrivmsgFn(target, msg) }
 
+// Join delegates to JoinFn
 func (m *MockBot) Join(target string) { m.JoinFn(target) }
+
+// Part delegates to PartFn
 func (m *MockBot) Part(target string) { m.PartFn(target) }
 
+// Wait is just here for the interface
 func (m *MockBot) Wait() <-chan struct{} { return nil }
 
+// AddEvent  is just here for the interface
 func (m *MockBot) AddEvent(ev noye.Event) {}
