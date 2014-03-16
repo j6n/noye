e6651f698288a4ffa48cb7055d2706157c7b3db3
j6n <genivf@outlook.com>
2014-03-16 02:41:29 -0400

    documented matcher

--- plugin/matcher.go
+++ plugin/matcher.go
@@ -2,16 +2,21 @@ package plugin
 
 import "regexp"
 
+// Matcher is a function that takes a string and
+// returns whether it matched and the matched string
 type Matcher func(string) (bool, string)
 
+// NoopMatcher is a matcher that does nothing
 func NoopMatcher() Matcher {
 	return func(s string) (bool, string) { return true, s }
 }
 
+// SimpleMatcher is a matcher that matches the input to 'in'
 func SimpleMatcher(in string) Matcher {
 	return StringMatcher(in, false)
 }
 
+// StringMatcher is a matcher which also captures the input
 func StringMatcher(in string, capture bool) Matcher {
 	return func(s string) (bool, string) {
 		ok := s == in
@@ -23,6 +28,7 @@ func StringMatcher(in string, capture bool) Matcher {
 	}
 }
 
+// RegexMatcher uses a regex to create a matcher
 func RegexMatcher(re *regexp.Regexp, capture bool) Matcher {
 	return func(s string) (bool, string) {
 		ok := re.MatchString(s)
