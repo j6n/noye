cb481ac481e18a160b6976e81c60a31afa7323a0
j6n <genivf@outlook.com>
2014-04-06 02:06:47 -0400

    added mock conn

---
+++ mock/conn.go
@@ -0,0 +1,40 @@
+package mock
+
+// MockConn is a type that implements noye.Conn
+// which can have parts of it overridden by changing the functors
+type MockConn struct {
+	DialFn      func(string) error
+	CloseFn     func()
+	WriteLineFn func(string)
+	ReadLineFn  func() (string, error)
+}
+
+// NewMockConn returns a new MockConn with the fns set to no-ops
+func NewMockConn() *MockConn {
+	return &MockConn{
+		func(string) error { return nil },
+		func() {},
+		func(string) {},
+		func() (string, error) { return "", nil },
+	}
+}
+
+// Dial delegates to DialFn
+func (m *MockConn) Dial(addr string) error {
+	return m.DialFn(addr)
+}
+
+// Close delegates to CloseFn
+func (m *MockConn) Close() {
+	m.Close()
+}
+
+// WriteLine delegates to WriteLineFn
+func (m *MockConn) WriteLine(raw string) {
+	m.WriteLineFn(raw)
+}
+
+// ReadLine delegates to ReadLineFn
+func (m *MockConn) ReadLine() (string, error) {
+	return m.ReadLineFn()
+}
